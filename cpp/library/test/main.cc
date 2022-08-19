1d2050b3a576927b37519d9ed9054ba782869f0c
Jatom <wenjun.huang80@gmail.com>
2022-08-19 18:31:07 +0800

    [c++] add unit test

---
+++ cpp/library/test/main.cc
@@ -0,0 +1,8 @@
+//
+// Created by rick on 8/19/2022.
+//
+#include <gtest/gtest.h>
+int main(){
+  RUN_ALL_TESTS();
+
+}
---
+++ cpp/library/test/scanner_spec.cc
@@ -0,0 +1,9 @@
+//
+// Created by rick on 8/19/2022.
+//
+
+#include <gtest/gtest.h>
+
+TEST(ScannerTest,"scanner should scan empty source") {
+  ASSERT_TRUE(true);
+}
