eaa56ed487562405922fa23432fe3dae98713aca
jatom <wenjun.huang80@gmail.com>
2022-09-22 23:58:55 +0800

    [scala] working on formatter

--- scala/ideaPlugin/src/main/scala/com/github/wenjunhuang/lox/ideaplugin/LoxFormattingModelBuilder.scala
+++ scala/ideaPlugin/src/main/scala/com/github/wenjunhuang/lox/ideaplugin/LoxFormattingModelBuilder.scala
@@ -1,12 +1,14 @@
 package com.github.wenjunhuang.lox.ideaplugin
 import com.intellij.formatting.{FormattingContext, FormattingModel, FormattingModelBuilder}
+import com.intellij.psi.formatter.DocumentBasedFormattingModel
 
 class LoxFormattingModelBuilder extends FormattingModelBuilder:
   override def createModel(formattingContext: FormattingContext): FormattingModel =
     val psiFile = formattingContext.getContainingFile
     val settings = formattingContext.getCodeStyleSettings
     val rootNode = formattingContext.getNode
-    val context =
+
+    DocumentBasedFormattingModel()
 
 end LoxFormattingModelBuilder
 
---
+++ scala/ideaPlugin/src/main/scala/com/github/wenjunhuang/lox/ideaplugin/formatter/LoxFormattingModelBuilder.scala
@@ -0,0 +1,20 @@
+package com.github.wenjunhuang.lox.ideaplugin.formatter
+
+import com.github.wenjunhuang.lox.ideaplugin.{Lox, LoxTypes}
+import com.intellij.formatting.{FormattingContext, FormattingModel, FormattingModelBuilder, SpacingBuilder}
+import com.intellij.psi.codeStyle.CodeStyleSettings
+
+class LoxFormattingModelBuilder extends FormattingModelBuilder:
+  override def createModel(formattingContext: FormattingContext): FormattingModel = super.createModel(formattingContext)
+end LoxFormattingModelBuilder
+
+object LoxFormattingModelBuilder:
+  def createSpaceBuilder(settings:CodeStyleSettings):SpacingBuilder =
+    SpacingBuilder(settings,Lox)
+      .around(LoxTypes.COMMA)
+      .spaceIf(settings.getCommonSettings(Lox.getID))
+      .before(LoxTypes.SEMICOLON)
+
+
+
+end LoxFormattingModelBuilder
